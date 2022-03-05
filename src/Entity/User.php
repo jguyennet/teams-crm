a35c2a9b2b73636074a692cbd6f465a0ccfc366f
Jerome Gyt <jerome.guyennet@orange.fr>
2022-03-05 15:27:44 +0100

    Après création de registration sans created_at

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,12 +4,14 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -35,6 +37,21 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $personal_email;
 
     public function getId(): ?int
     {
@@ -123,5 +140,41 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getPersonalEmail(): ?string
+    {
+        return $this->personal_email;
+    }
+
+    public function setPersonalEmail(string $personal_email): self
+    {
+        $this->personal_email = $personal_email;
+
+        return $this;
     }
 }
