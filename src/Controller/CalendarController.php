470a44c9cc44b3b7959d1ef167aff04f5ad9d4a7
Jerome Gyt <jerome.guyennet@orange.fr>
2022-03-09 15:40:25 +0100

    20:42 Avant modif de CalendarController

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CalendarController extends AbstractController
+class CalendarController extends Controller
 {
     /**
      * @Route("/calendar", name="app_calendar")
