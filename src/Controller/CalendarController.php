d41b24825bfab4d838977828bd9cd8f40d87afc7
Jerome Gyt <jerome.guyennet@orange.fr>
2022-03-09 16:33:34 +0100

    Avant 32:30 / Avant creation de ApiController qui permet grace a ajax de modifier notre BDD quand on modif le calendar / Après possibilité de bouger les evenements

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,28 @@ class CalendarController extends Controller
     /**
      * @Route("/calendar", name="app_calendar")
      */
-    public function index(): Response
+    public function index(EventRepository $calendar): Response
     {
-        return $this->render('calendar/index.html.twig', [
-            'controller_name' => 'CalendarController',
-        ]);
+        $events = $calendar->findAll();
+        
+        $rdvs = [];
+
+        foreach($events as $event){
+            $rdvs[] = [
+                'id' => $event->getId(),
+                'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                'title' => $event->getTitle(),
+                'description' => $event->getDescription(),
+                'backgroundColor' => $event->getBackgroundColor(),
+                'borderColor' => $event->getBorderColor(),
+                'textColor' => $event->getTextColor(),
+                'allDay' => $event->getAllDay(),
+            ];
+        }
+
+        $data = json_encode($rdvs);
+
+        return $this->render('calendar/index.html.twig', compact('data'));
     }
 }
